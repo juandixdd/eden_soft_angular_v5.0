55b1442e41a23eccd87f75a242139dbc4932a9dd
Daniel <jdps106@hotmail.com>
2022-05-26 11:43:06 -0500

    se agrego tabla de ventas jijijiji

--- src/app/modules/main/dashboard/ventas-table/ventas-table.component.ts
+++ src/app/modules/main/dashboard/ventas-table/ventas-table.component.ts
@@ -1,15 +1,250 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
 
+import {
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexStroke,
+  ApexDataLabels,
+  ApexXAxis,
+  ApexGrid,
+  ApexTitleSubtitle,
+  ApexTooltip,
+  ApexPlotOptions,
+  ApexYAxis,
+  ApexFill,
+  ApexMarkers,
+  ApexTheme,
+  ApexNonAxisChartSeries,
+  ApexLegend,
+  ApexResponsive,
+  ApexStates
+} from 'ng-apexcharts';
+
+import { colors } from 'app/colors.const';
+import { CoreConfigService } from '@core/services/config.service';
+
+// interface ChartOptions
+export interface ChartOptions {
+  series?: ApexAxisChartSeries;
+  chart?: ApexChart;
+  xaxis?: ApexXAxis;
+  dataLabels?: ApexDataLabels;
+  grid?: ApexGrid;
+  stroke?: ApexStroke;
+  legend?: ApexLegend;
+  title?: ApexTitleSubtitle;
+  colors?: string[];
+  tooltip?: ApexTooltip;
+  plotOptions?: ApexPlotOptions;
+  yaxis?: ApexYAxis;
+  fill?: ApexFill;
+  labels?: string[];
+  markers: ApexMarkers;
+  theme: ApexTheme;
+}
+
+export interface ChartOptions2 {
+  // Apex-non-axis-chart-series
+  series?: ApexNonAxisChartSeries;
+  chart?: ApexChart;
+  stroke?: ApexStroke;
+  tooltip?: ApexTooltip;
+  dataLabels?: ApexDataLabels;
+  fill?: ApexFill;
+  colors?: string[];
+  legend?: ApexLegend;
+  labels?: any;
+  plotOptions?: ApexPlotOptions;
+  responsive?: ApexResponsive[];
+  markers?: ApexMarkers[];
+  xaxis?: ApexXAxis;
+  yaxis?: ApexYAxis;
+  states?: ApexStates;
+}
 @Component({
   selector: 'app-ventas-table',
   templateUrl: './ventas-table.component.html',
-  styleUrls: ['./ventas-table.component.scss']
+  styleUrls: ['./ventas-table.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class VentasTableComponent implements OnInit {
+  @ViewChild('apexLineChartRef') apexLineChartRef: any;
 
-  constructor() { }
+  public contentHeader: object;
+  public apexLineChart: Partial<ChartOptions>;
+  public isMenuToggled = false;
+  public radioModel = 1;
+
+   // ng2-flatpickr options
+   public DateRangeOptions = {
+    altInput: true,
+    mode: 'range',
+    altInputClass: 'form-control flat-picker bg-transparent border-0 shadow-none flatpickr-input',
+    defaultDate: ['2019-05-01', '2019-05-10'],
+    altFormat: 'Y-n-j'
+  };
+
+  // Color Variables
+  chartColors = {
+    column: {
+      series1: '#826af9',
+      series2: '#d2b0ff',
+      bg: '#f8d3ff'
+    },
+    success: {
+      shade_100: '#7eefc7',
+      shade_200: '#06774f'
+    },
+    donut: {
+      series1: '#ffe700',
+      series2: '#00d4bd',
+      series3: '#826bf8',
+      series4: '#2b9bf4',
+      series5: '#FFA1A1'
+    },
+    area: {
+      series3: '#a4f8cd',
+      series2: '#60f2ca',
+      series1: '#2bdac7'
+    }
+  };
+
+   // Heatmap data generate
+   public generateHeatmapData(count, yrange) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+      var x = 'w' + (i + 1).toString();
+      var y = Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+      series.push({
+        x: x,
+        y: y
+      });
+      i++;
+    }
+    return series;
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {CoreConfigService} _coreConfigService
+   */
+
+  constructor(private _coreConfigService: CoreConfigService) {
+     // Apex Line Area Chart
+     this.apexLineChart = {
+      series: [
+        {
+          data: [280, 200, 220, 180, 270, 250, 70, 90, 200, 150, 160, 100, 150, 100, 50]
+        }
+      ],
+      chart: {
+        height: 400,
+        type: 'line',
+        zoom: {
+          enabled: false
+        },
+        toolbar: {
+          show: false
+        }
+      },
+      grid: {
+        xaxis: {
+          lines: {
+            show: true
+          }
+        }
+      },
+      markers: {
+        strokeWidth: 7,
+        strokeOpacity: 1,
+        strokeColors: [colors.solid.white],
+        colors: [colors.solid.warning]
+      },
+      colors: [colors.solid.warning],
+      dataLabels: {
+        enabled: false
+      },
+      stroke: {
+        curve: 'straight'
+      },
+      xaxis: {
+        categories: [
+          '7/12',
+          '8/12',
+          '9/12',
+          '10/12',
+          '11/12',
+          '12/12',
+          '13/12',
+          '14/12',
+          '15/12',
+          '16/12',
+          '17/12',
+          '18/12',
+          '19/12',
+          '20/12',
+          '21/12'
+        ]
+      },
+      tooltip: {
+        custom: function (data) {
+          return (
+            '<div class="px-1 py-50">' +
+            '<span>' +
+            data.series[data.seriesIndex][data.dataPointIndex] +
+            '%</span>' +
+            '</div>'
+          );
+        }
+      }
+    };
+   }
 
   ngOnInit(): void {
+    this.contentHeader = {
+      headerTitle: 'Apex Charts',
+      actionButton: true,
+      breadcrumb: {
+        type: '',
+        links: [
+          {
+            name: 'Home',
+            isLink: true,
+            link: '/'
+          },
+          {
+            name: 'Table',
+            isLink: true,
+            link: '/'
+          },
+          {
+            name: 'Apex Charts',
+            isLink: false
+          }
+        ]
+      }
+    };
+  }
+
+  /**
+   * After View Init
+   */
+   ngAfterViewInit() {
+    // Subscribe to core config changes
+    this._coreConfigService.getConfig().subscribe(config => {
+      // If Menu Collapsed Changes
+      if (config.layout.menu.collapsed === true || config.layout.menu.collapsed === false) {
+        setTimeout(() => {
+          // Get Dynamic Width for Charts
+          
+          this.apexLineChart.chart.width = this.apexLineChartRef?.nativeElement.offsetWidth;
+          
+        }, 900);
+      }
+    });
   }
 
 }

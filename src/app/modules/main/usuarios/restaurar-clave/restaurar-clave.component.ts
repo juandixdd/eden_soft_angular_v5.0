588b5e52b1d07b3fca2128d53ac84613d2ca3513
Daniel <jdps106@hotmail.com>
2022-05-27 09:41:05 -0500

    se agrego vista de restaurar contraseña jijiji

--- src/app/modules/main/usuarios/restaurar-clave/restaurar-clave.component.ts
+++ src/app/modules/main/usuarios/restaurar-clave/restaurar-clave.component.ts
@@ -1,15 +1,106 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+
+import { CoreConfigService } from '@core/services/config.service';
 
 @Component({
   selector: 'app-restaurar-clave',
   templateUrl: './restaurar-clave.component.html',
-  styleUrls: ['./restaurar-clave.component.scss']
+  styleUrls: ['./restaurar-clave.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class RestaurarClaveComponent implements OnInit {
 
-  constructor() { }
+
+
+export class RestaurarClaveComponent implements OnInit {
+  // Public
+  public coreConfig: any;
+  public passwordTextType: boolean;
+  public confPasswordTextType: boolean;
+  public resetPasswordForm: FormGroup;
+  public submitted = false;
+
+  // Private
+  private _unsubscribeAll: Subject<any>;
+
+  /**
+   * Constructor
+   *
+   * @param {CoreConfigService} _coreConfigService
+   * @param {FormBuilder} _formBuilder
+   */
+
+  constructor(private _coreConfigService: CoreConfigService, private _formBuilder: FormBuilder) {
+    this._unsubscribeAll = new Subject();
+
+    // Configure the layout
+   /* this._coreConfigService.config = {
+      layout: {
+        navbar: {
+          hidden: true
+        },
+        menu: {
+          hidden: true
+        },
+        footer: {
+          hidden: true
+        },
+        customizer: false,
+        enableLocalStorage: false
+      }
+    };*/
+   }
+
+   get f() {
+    return this.resetPasswordForm.controls;
+  }
+
+  /**
+   * Toggle password
+   */
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
+  }
+
+  /**
+   * Toggle confirm password
+   */
+  toggleConfPasswordTextType() {
+    this.confPasswordTextType = !this.confPasswordTextType;
+  }
+
+  /**
+   * On Submit
+   */
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.resetPasswordForm.invalid) {
+      return;
+    }
+  }
+
+
 
   ngOnInit(): void {
+    this.resetPasswordForm = this._formBuilder.group({
+      newPassword: ['', [Validators.required]],
+      confirmPassword: ['', [Validators.required]]
+    });
+
+    // Subscribe to config changes
+    this._coreConfigService.config.pipe(takeUntil(this._unsubscribeAll)).subscribe(config => {
+      this.coreConfig = config;
+    });
+  }
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
   }
 
 }

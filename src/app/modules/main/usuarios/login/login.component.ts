6d91abd6c55a640d52ba0a2a21ec611972afba6a
Daniel <jdps106@hotmail.com>
2022-06-01 10:05:52 -0500

    se quiere realizar una prueba de iconos

--- src/app/modules/main/usuarios/login/login.component.ts
+++ src/app/modules/main/usuarios/login/login.component.ts
@@ -1,15 +1,97 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { takeUntil, first } from 'rxjs/operators';
+import { Subject } from 'rxjs';
 
+
+import { CoreConfigService } from '@core/services/config.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class LoginComponent implements OnInit {
+  //  Public
+  public coreConfig: any;
+  public loginForm: FormGroup;
+  public loading = false;
+  public submitted = false;
+  public returnUrl: string;
+  public error = '';
+  public passwordTextType: boolean;
 
-  constructor() { }
+  // Private
+  private _unsubscribeAll: Subject<any>;
 
-  ngOnInit(): void {
+  /**
+   * Constructor
+   *
+   * @param {CoreConfigService} _coreConfigService
+   */
+  constructor(
+    private _coreConfigService: CoreConfigService,
+    private _formBuilder: FormBuilder,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    
+  ) {
+    
+
+    this._unsubscribeAll = new Subject();
+
+    // Configure the layout
+    this._coreConfigService.config = {
+      layout: {
+        navbar: {
+          hidden: false
+        },
+        menu: {
+          hidden: false
+        },
+        footer: {
+          hidden: false
+        },
+        customizer: false,
+        enableLocalStorage: false
+      }
+    };
+  }
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
   }
 
+  /**
+   * Toggle password
+   */
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    // Login
+    this.loading = true;
+    
+  }
+
+  ngOnInit(): void {
+   
+  }
+  /**
+     * On destroy
+     */
+  ngOnDestroy(): void {
+    // Unsubscribe from all subscriptions
+    this._unsubscribeAll.next();
+    this._unsubscribeAll.complete();
+  }
 }

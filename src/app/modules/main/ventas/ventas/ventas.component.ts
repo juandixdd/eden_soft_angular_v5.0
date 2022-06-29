364560c440dd66f247e01ed33859ca96d3a84b2f
Daniel <jdps106@hotmail.com>
2022-06-29 11:17:29 -0500

    Se agrega modulo de ventas

--- src/app/modules/main/ventas/ventas/ventas.component.ts
+++ src/app/modules/main/ventas/ventas/ventas.component.ts
@@ -1,15 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ColumnMode } from '@swimlane/ngx-datatable';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-ventas',
   templateUrl: './ventas.component.html',
-  styleUrls: ['./ventas.component.scss']
+  styleUrls: ['./ventas.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class VentasComponent implements OnInit {
 
-  constructor() { }
+  public selectedOption = 10; //? Este es el selector de cuantas filas quieres ver en la tabla, en este caso, 10.
+  public ColumnMode = ColumnMode; //? Esto es para que cuando selecciones una fila, se seleccione la fila y no el boton.
+  private tempData = []; //? Estas son cosas del buiscador (Que no funciona)
+  public kitchenSinkRows: any;
+  
+  rows: any = [ //? Estos son los datos de la tabla quemados.
+    {
+      id: 1,
+      date: '01/01/2020',
+      valor: '$350.000',
+    },
+    
+  ]
+
+  constructor(
+    private modalService: NgbModal, //? Aquí se instancia el servicio para abrir la modal.
+  ) { }
 
   ngOnInit(): void {
+    this.tempData = this.rows; //? Esto también es del buscador (Que no funciona)
+    this.kitchenSinkRows = this.rows;
   }
+
+  modalOpen(modal) { //? Esta es la funcion que abre las modales.
+    this.modalService.open(modal, {
+      centered: true,
+    });
+  }
+
+  confirmAlert() { //? Esta es la funcion que abre el sweetAlert de confirmacion.
+    Swal.fire({
+      title: '¿Estas seguro?',
+      text: "Si desactivas este rol, desactivaras a todos los usuarios que pertenecen a este rol!",
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#3085d6',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, desactivar!',
+      reverseButtons: true
+    }).then((result) => {
+      if (result.value) {
+        Swal.fire(
+          'Desactivado!',
+          'Este rol ha sido desactivado.',
+          'success'
+        )
+      }
+    })
+  }
+
+  filterUpdate(event) { //? Buscador, no le pare bolas.
+    const val = event.target.value.toLowerCase();
+
+    const temp = this.tempData.filter(function (d) {
+      return d.name.toLowerCase().indexOf(val) !== -1 || !val;
+    });
+
+    // update the rows
+    this.kitchenSinkRows = temp;
+  }
+
+
+
 
 }

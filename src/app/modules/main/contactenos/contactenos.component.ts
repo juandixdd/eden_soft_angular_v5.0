9b0be4c90831d7c349faf73da6854c67333e9679
JuanDi <juandixarteagaperez210@gmail.com>
2022-06-28 22:44:10 -0500

    Se agrega header

--- src/app/modules/main/contactenos/contactenos.component.ts
+++ src/app/modules/main/contactenos/contactenos.component.ts
@@ -1,4 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { takeUntil } from 'rxjs/operators';
+import { Subject } from 'rxjs';
+import { Router } from '@angular/router';
+import { CoreConfigService } from '@core/services/config.service';
+import Swal from 'sweetalert2';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-contactenos',
@@ -7,7 +15,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactenosComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private _coreConfigService: CoreConfigService,
+    private _formBuilder: FormBuilder,
+    private _route: ActivatedRoute,
+    private _router: Router,
+    private modalService: NgbModal,
+  ) {
+
+    // Configure the layout
+    this._coreConfigService.config = {
+      layout: {
+        navbar: {
+          hidden: true
+        },
+        menu: {
+          hidden: true
+        },
+        footer: {
+          hidden: true
+        },
+        customizer: false,
+        enableLocalStorage: false
+      }
+    };
+  }
 
   ngOnInit(): void {
   }

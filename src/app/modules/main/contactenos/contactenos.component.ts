48e43c523000d9d7164200bf49295efef71fb65f
Daniel Peña <jdps106@hotmail.com>
2022-07-29 11:21:29 -0500

    se actualizo a medias el landing

--- src/app/modules/main/contactenos/contactenos.component.ts
+++ src/app/modules/main/contactenos/contactenos.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CoreConfigService } from '@core/services/config.service';
+import { Subject } from 'rxjs';
 
 @Component({
   selector: 'app-contactenos',
@@ -6,8 +8,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contactenos.component.scss']
 })
 export class ContactenosComponent implements OnInit {
+  private _unsubscribeAll: Subject<any>;
 
-  constructor() { }
+  constructor(
+    private _coreConfigService: CoreConfigService,
+
+  ) {
+    this._unsubscribeAll = new Subject();
+
+    // Configure the layout
+    this._coreConfigService.config = {
+      layout: {
+        navbar: {
+          hidden: true
+        },
+        menu: {
+          hidden: true
+        },
+        footer: {
+          hidden: false
+        },
+        customizer: false,
+        enableLocalStorage: false
+      }
+    };
+   }
 
   ngOnInit(): void {
   }

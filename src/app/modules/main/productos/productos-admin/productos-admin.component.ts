897c0604ec2a1e283e8d33b646fe8905c08416b8
JuanDi <juandixarteagaperez210@gmail.com>
2022-08-31 10:41:20 -0500

    Se cambia modal de info de productos

--- src/app/modules/main/productos/productos-admin/productos-admin.component.ts
+++ src/app/modules/main/productos/productos-admin/productos-admin.component.ts
@@ -1,21 +1,20 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { ColumnMode } from '@swimlane/ngx-datatable';
-import { CategoriaService } from 'app/modules/services/categoria/categoria.service';
-import { ProductosService } from 'app/modules/services/productos/productos.service';
-import { of } from 'rxjs';
-import { map } from 'rxjs/operators';
-import Swal from 'sweetalert2';
+import { Component, OnInit, ViewEncapsulation } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
+import { ColumnMode } from "@swimlane/ngx-datatable";
+import { CategoriaService } from "app/modules/services/categoria/categoria.service";
+import { ProductosService } from "app/modules/services/productos/productos.service";
+import { of } from "rxjs";
+import { map } from "rxjs/operators";
+import Swal from "sweetalert2";
 
 @Component({
-  selector: 'app-productos-admin',
-  templateUrl: './productos-admin.component.html',
-  styleUrls: ['./productos-admin.component.scss'],
-  encapsulation: ViewEncapsulation.None
+  selector: "app-productos-admin",
+  templateUrl: "./productos-admin.component.html",
+  styleUrls: ["./productos-admin.component.scss"],
+  encapsulation: ViewEncapsulation.None,
 })
 export class ProductosAdminComponent implements OnInit {
-
   //! Oes, cuando no entiendan para que es una cosa, le dan ctrl + f para buscar en el codigo de html, tal vez ahí tengan una mjeor noción de para que sirve cada cosa.
 
   public selectedOption = 10; //? Este es el selector de cuantas filas quieres ver en la tabla, en este caso, 10.
@@ -23,49 +22,46 @@ export class ProductosAdminComponent implements OnInit {
   private tempData = []; //? Estas son cosas del buiscador (Que no funciona)
   public kitchenSinkRows: any;
 
-
-
   categorias: any;
-  rows: any = []
-  product: any = {}
+  rows: any = [];
+  product: any = {};
   selectMultiSelectedEvent: any;
   selectMultiSelected: any;
   selectBasic: any;
   categoryId: any;
   imgUrl: any;
-  imgName:any;
-  row:any;
-
+  imgName: any;
+  row: any;
 
   constructor(
     private modalService: NgbModal,
     private productosService: ProductosService,
     private categoriasService: CategoriaService,
     private fb: FormBuilder
-  ) { }
+  ) {}
 
   public productForm: FormGroup = this.fb.group({
     nombre: [
       "",
-      [Validators.required, Validators.minLength(3), Validators.maxLength(100)]
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
     ],
     precio: [
       "",
-      [Validators.required, Validators.minLength(3), Validators.maxLength(100)]
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
     ],
     categoria: [
       "",
-      [Validators.required, Validators.minLength(3), Validators.maxLength(100)]
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
     ],
     imagen: [
       "",
-      [Validators.required, Validators.minLength(3), Validators.maxLength(100)]
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
     ],
     estado: [
       "",
-      [Validators.required, Validators.minLength(3), Validators.maxLength(100)]
-    ]
-  })
+      [Validators.required, Validators.minLength(3), Validators.maxLength(100)],
+    ],
+  });
 
   ngOnInit(): void {
     this.getProducts();
@@ -73,35 +69,33 @@ export class ProductosAdminComponent implements OnInit {
     this.getCategorias();
   }
 
-  modalOpen(modal) { //? Esta es la funcion que abre las modales.
+  modalOpen(modal) {
+    //? Esta es la funcion que abre las modales.
     this.modalService.open(modal, {
       centered: true,
     });
   }
 
   getProducts() {
-    this.productosService.getData().subscribe(
-      (res) => {
-        this.rows = res;
-        console.log(this.rows);
-      }
-    )
+    this.productosService.getData().subscribe((res) => {
+      this.rows = res;
+      console.log(this.rows);
+    });
   }
 
   onChange(event) {
     this.categoryId = event.id;
-    console.log(this.categoryId)
+    console.log(this.categoryId);
   }
 
   options: any = [];
 
   async getCategorias() {
-    this.categoriasService.getData().subscribe(
-      (res: any) => {
-        this.selectBasic = of(res).pipe();
-        console.log(this.selectBasic);
+    this.categoriasService.getData().subscribe((res: any) => {
+      this.selectBasic = of(res).pipe();
+      console.log(this.selectBasic);
 
-        /*
+      /*
 
         for (let i in this.selectBasic) {
           console.log(this.selectBasic[i].nombre);
@@ -110,83 +104,72 @@ export class ProductosAdminComponent implements OnInit {
 
         console.log(this.options)
         */
-
-      }
-    )
+    });
   }
 
-  defineImg(id){
+  productInfo = {};
+
+  defineProductInfo(id) {
     this.productosService.getDataById(id).subscribe(
       (res:any)=>{
-        this.imgUrl = res[0].imagen
-        this.imgName = res[0].nombre + "Img"
+        this.productInfo=res[0]
       }
     )
   }
 
   createProduct() {
-
     this.product = {
       nombre: this.productForm.value.nombre,
       precio: this.productForm.value.precio,
       categoria: this.categoryId,
       imagen: this.productForm.value.imagen,
-      estado: 0
-    }
-
+      estado: 1,
+    };
 
     this.productosService.createProduct(this.product).subscribe(
       (res: any) => {
         console.log(res);
         Swal.fire({
-          position: 'top-end',
-          icon: 'success',
-          title: 'Producto creado',
-          text: 'El producto se ha creado correctamente',
+          position: "top-end",
+          icon: "success",
+          title: "Producto creado",
+          text: "El producto se ha creado correctamente",
           showConfirmButton: false,
-          timer: 1000
-        })
+          timer: 1000,
+        });
 
         this.modalService.dismissAll();
         this.productForm.reset();
         this.getProducts();
-
       },
       (err: any) => {
-        console.log("No se pudo guardar")
+        console.log("No se pudo guardar");
         console.log(err);
         Swal.fire({
-          icon: 'error',
-          title: 'Oops...',
-          text: 'Ha ocurrido un error, por favor intente nuevamente',
-          confirmButtonText: 'Ok'
-        })
+          icon: "error",
+          title: "Oops...",
+          text: "Ha ocurrido un error, por favor intente nuevamente",
+          confirmButtonText: "Ok",
+        });
       }
-    )
-
+    );
   }
 
-  confirmAlert() { //? Esta es la funcion que abre el sweetAlert de confirmacion.
+  confirmAlert() {
+    //? Esta es la funcion que abre el sweetAlert de confirmacion.
     Swal.fire({
-      title: '¿Estas seguro?',
+      title: "¿Estas seguro?",
       text: "No podras revertir esto!",
-      icon: 'warning',
+      icon: "warning",
       showCancelButton: true,
-      confirmButtonColor: '#3085d6',
-      cancelButtonColor: '#d33',
-      confirmButtonText: 'Si, eliminar!',
-      reverseButtons: true
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Si, eliminar!",
+      reverseButtons: true,
     }).then((result) => {
       if (result.value) {
-        Swal.fire(
-          'Eliminado!',
-          'El producto ha sido eliminada.',
-          'success'
-        )
+        Swal.fire("Eliminado!", "El producto ha sido eliminada.", "success");
       }
-    })
+    });
   }
-
-
-
 }

f0c07a3539fe32120f8543a01d978c5fcf79dbda
lizee <llsandoval7@misena.edu.co>
2022-09-14 11:30:58 -0500

    se agrega modal de crear cotizacion

--- src/app/modules/main/productos/productos/productos.component.ts
+++ src/app/modules/main/productos/productos/productos.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 import { CoreConfigService } from '@core/services/config.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { LoginService } from 'app/modules/services/login/login.service';
 import { ProductosService } from 'app/modules/services/productos/productos.service';
+import { UsersService } from 'app/modules/services/users/users.service';
 import { count } from 'console';
+import moment, { parseTwoDigitYear } from 'moment';
 import { parse } from 'path';
+import { Subject } from 'rxjs';
 import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-productos',
   templateUrl: './productos.component.html',
-  styleUrls: ['./productos.component.scss']
+  styleUrls: ['./productos.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class ProductosComponent implements OnInit {
 
@@ -19,11 +26,19 @@ export class ProductosComponent implements OnInit {
   wishListIds: any = [];
   productxd = [];
   public items = JSON.parse(localStorage.getItem("wishList")) || [];
+  // Private
+  private _unsubscribeAll: Subject<any>;
+  user: any = {};
+  public passwordTextType: boolean;
 
   constructor(
     private _coreConfigService: CoreConfigService,
     private productosService: ProductosService,
+    private usersService: UsersService,
     private modalService: NgbModal,
+    private fb: FormBuilder,
+    private loginService: LoginService,
+    private router: Router
   ) {
     // Configure the layout
     this._coreConfigService.config = {
@@ -49,6 +64,17 @@ export class ProductosComponent implements OnInit {
       centered: true,
     });
   }
+
+  public loginForm: FormGroup = this.fb.group({
+    email: [
+      "",
+      [Validators.required, Validators.email],
+    ],
+    password: [
+      "",
+      [Validators.required, Validators.minLength(3), Validators.maxLength(30)],
+    ],
+  })
 
   ngOnInit(): void {
     this.getProducts();
@@ -123,6 +149,7 @@ export class ProductosComponent implements OnInit {
     }
   }
 
+
   /**
    * DeleteItem
    *
@@ -137,6 +164,56 @@ export class ProductosComponent implements OnInit {
     }
   }
 
+  generarCotizacion(item){
+    let usuario;
+    this.usersService.getDataById(item.userId).subscribe(
+      (res:any)=>{
+        usuario = res[0];
+        console.log(usuario)
+        
+        let newItem = {
+          id_usuario_documento: usuario.id,
+          fecha_registro: moment().format('YYYY-MM-D'),
+          estado: '',
+          pago: 0
+        }
 
+        console.log(newItem)
+      }
+    )
+ 
+  }
+
+  togglePasswordTextType() {
+    this.passwordTextType = !this.passwordTextType;
+  }
+
+  //!************************** funcion de login *********************************
+  userData: any;
+  loginUser() {
+
+    this.user.email = this.loginForm.controls['email'].value;
+    this.user.password = this.loginForm.controls['password'].value;
+
+    this.loginService.login(this.user).subscribe(
+      (res: any) => {
+        if (res.statusCode == 200) {
+          console.log("Login exitoso")
+          this.generarCotizacion(res)
+          this.modalService.dismissAll();
+          localStorage.setItem('token', res.token);
+          localStorage.setItem('userId', res.userId);
+        } else {
+
+          Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'El usuario o la contraseña son incorrectos'
+          })
+        }
+      }
+    )
+
+  }
 
 }

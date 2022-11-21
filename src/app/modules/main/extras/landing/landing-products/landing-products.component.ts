bf4fe0e9dd2a709d932f7d26e1631cb14f2d390f
JuanDi <juandixarteagaperez210@gmail.com>
2022-11-21 08:51:10 -0500

    Se arregla error de top3 y se agrega boton de descargar informe

--- src/app/modules/main/extras/landing/landing-products/landing-products.component.ts
+++ src/app/modules/main/extras/landing/landing-products/landing-products.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { DashboardService } from "app/modules/services/dashboard/dashboard.service";
 import { ProductosService } from "app/modules/services/productos/productos.service";
-import { table } from "console";
+import * as reader from 'xlsx';
 
 @Component({
   selector: "app-landing-products",
@@ -11,154 +11,172 @@ import { table } from "console";
 export class LandingProductsComponent implements OnInit {
   topVentas = [];
   topPedidos = [];
-  topPedidosLocales=[];
+  topPedidosLocales = [];
   totalesTop = [];
-  top3=[];
+  top3 = [];
 
-  dia=new Date().getDate();
-  mes=new Date().getMonth()+1;
-  anio=new Date().getFullYear();
-  mesName=new Intl.DateTimeFormat('es-ES', { month: 'long'}).format(new Date());
-  fecha= this.anio+'-'+this.mes+'-'+this.dia;
-  initialData={
-    mes:this.mes,
-    anio:this.anio
-  }
+  dia = new Date().getDate();
+  mes = new Date().getMonth() + 1;
+  anio = new Date().getFullYear();
+  mesName = new Intl.DateTimeFormat("es-ES", { month: "long" }).format(
+    new Date()
+  );
+  fecha = this.anio + "-" + this.mes + "-" + this.dia;
+  initialData = {
+    mes: this.mes,
+    anio: this.anio,
+  };
 
   constructor(
     private productosService: ProductosService,
     private dashboardService: DashboardService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
-   
     this.getTotalesTop();
     console.warn(this.fecha);
     console.log(this.mesName);
     this.getDataPedidos(this.initialData);
     this.getDataPedidosLocales(this.initialData);
     this.getDataVentas(this.initialData);
-    
-
-   
   }
 
-  getMonth(event){
-    let value=event.target.value
-    this.mes=value
-    
-    let body={
-      mes:this.mes,
-      anio:this.anio
-    }
+  getMonth(event) {
+    let value = event.target.value;
+    this.mes = value;
+
+    let body = {
+      mes: this.mes,
+      anio: this.anio,
+    };
     this.getDataPedidos(body);
     this.getDataPedidosLocales(body);
     this.getDataVentas(body);
+    this.getTotalesTop();
   }
-  getYear(event){
-    let value=event.target.value
-    this.anio=value
-    
-    let body={
-      mes:this.mes,
-      anio:this.anio
-    }
+  getYear(event) {
+    let value = event.target.value;
+    this.anio = value;
+
+    let body = {
+      mes: this.mes,
+      anio: this.anio,
+    };
     this.getDataPedidos(body);
     this.getDataPedidosLocales(body);
     this.getDataVentas(body);
+    this.getTotalesTop();
   }
 
-  getDataPedidos(body){
-    this.dashboardService.getTopPedidosMEs(body).subscribe((res:any)=>{
+  getDataPedidos(body) {
+    this.dashboardService.getTopPedidosMEs(body).subscribe((res: any) => {
       console.log(res);
-      this.topPedidos=res;
-    
-    })
+      this.topPedidos = res;
+    });
   }
 
-  getDataPedidosLocales(body){
-    this.dashboardService.getTopPedidosLocalesMEs(body).subscribe((res:any)=>{
+  getDataPedidosLocales(body) {
+    this.dashboardService
+      .getTopPedidosLocalesMEs(body)
+      .subscribe((res: any) => {
+        console.log(res);
+        this.topPedidosLocales = res;
+      });
+  }
+  getDataVentas(body) {
+    this.dashboardService.getTopVentasMEs(body).subscribe((res: any) => {
       console.log(res);
-      this.topPedidosLocales=res;
-    
-    })
-  }
-  getDataVentas(body){
-    this.dashboardService.getTopVentasMEs(body).subscribe((res:any)=>{
-      console.log(res);
-      this.topVentas=res;
-    
-    })
+      this.topVentas = res;
+    });
   }
 
-getTotalesTop(){
-  setTimeout(() => {
-    this.totalesTop=this.totalesTop.concat(this.topPedidos,this.topPedidosLocales,this.topVentas);
 
-    console.table(this.totalesTop);
 
-  this.top3 = this.totalesTop.reduce((acc,valorActual) => {
-          let siExiste = acc.find(elemento => elemento.id_producto === valorActual.id_producto
-          );
-      
-          //si hay objetos
-          if (siExiste) {
-              return acc.map(elemento => {
-                  if (elemento.id_producto === valorActual.id_producto) {
-                      return {
-                          ...elemento,
-                          cantidad_venta: elemento.cantidad_venta + valorActual.cantidad_venta,
-                      };
-                  }
-                  return elemento
-              });
-          }
-          return [...acc, valorActual];
+  getTotalesTop() {
+    setTimeout(() => {
+      this.totalesTop = [];
+
+      this.totalesTop = this.totalesTop.concat(
+        this.topPedidos,
+        this.topPedidosLocales,
+        this.topVentas
+      );
+
+      console.log("Holaaaaa", this.totalesTop);
+
+      console.table(this.totalesTop);
+
+      this.top3 = this.totalesTop.reduce((acc, valorActual) => {
+        let siExiste = acc.find(
+          (elemento) => elemento.id_producto === valorActual.id_producto
+        );
+
+        //si hay objetos
+        if (siExiste) {
+          return acc.map((elemento) => {
+            if (elemento.id_producto === valorActual.id_producto) {
+              return {
+                ...elemento,
+                cantidad_venta:
+                  elemento.cantidad_venta + valorActual.cantidad_venta,
+              };
+            }
+            return elemento;
+          });
+        }
+        return [...acc, valorActual];
       }, []);
-      
-      this.top3.sort(((a, b) => b.cantidad_venta - a.cantidad_venta)),
-      
-      console.table(this.top3);
 
-  }, 100);
+      this.top3.sort((a, b) => b.cantidad_venta - a.cantidad_venta),
+        console.table("Este es el top3: ", this.top3);
+    }, 100);
+  }
+
+  exportExcel() {
+    let date = new Date().getMilliseconds()
+    let workBook = reader.utils.book_new()
+    const worksheet = reader.utils.json_to_sheet(this.top3)
+    reader.utils.book_append_sheet(workBook, worksheet, `informe`)
+    let exportFileName = `InformeVentas-${date}.xlsx`
+    reader.writeFile(workBook, exportFileName)
+  }
 }
-  
-}
+
 
 
 //------ Logida del top 3 ------
 
-  // const ventasLocales = [
-  //     { id: 1, Nombre: "buñuelo", Ventas: 30 },
-  //     { id: 2, Nombre: "Empanada", Ventas: 23 },
-  // ]
-  
-  // const Pedidos = [
-  //     { id: 1, Nombre: "buñuelo", Ventas: 32 },
-  //     { id: 3, Nombre: "Leche", Ventas: 44 },
-  //     { id: 2, Nombre: "Empanada", Ventas: 50 }
-  // ]
-  
-  // const array = ventasLocales.concat(Pedidos);
-  
-  
-  // let top3 = array.reduce((acc,valorActual) => {
-  //     let siExiste = acc.find(elemento => elemento.id === valorActual.id
-  //     );
-  
-  //     //si hay objetos
-  //     if (siExiste) {
-  //         return acc.map(elemento => {
-  //             if (elemento.id === valorActual.id) {
-  //                 return {
-  //                     ...elemento,
-  //                     Ventas: elemento.Ventas + valorActual.Ventas,
-  //                 };
-  //             }
-  //             return elemento
-  //         });
-  //     }
-  //     return [...acc, valorActual];
-  // }, []);
-  
-  // console.log(top3);
+// const ventasLocales = [
+//     { id: 1, Nombre: "buñuelo", Ventas: 30 },
+//     { id: 2, Nombre: "Empanada", Ventas: 23 },
+// ]
+
+// const Pedidos = [
+//     { id: 1, Nombre: "buñuelo", Ventas: 32 },
+//     { id: 3, Nombre: "Leche", Ventas: 44 },
+//     { id: 2, Nombre: "Empanada", Ventas: 50 }
+// ]
+
+// const array = ventasLocales.concat(Pedidos);
+
+// let top3 = array.reduce((acc,valorActual) => {
+//     let siExiste = acc.find(elemento => elemento.id === valorActual.id
+//     );
+
+//     //si hay objetos
+//     if (siExiste) {
+//         return acc.map(elemento => {
+//             if (elemento.id === valorActual.id) {
+//                 return {
+//                     ...elemento,
+//                     Ventas: elemento.Ventas + valorActual.Ventas,
+//                 };
+//             }
+//             return elemento
+//         });
+//     }
+//     return [...acc, valorActual];
+// }, []);
+
+// console.log(top3);
+
